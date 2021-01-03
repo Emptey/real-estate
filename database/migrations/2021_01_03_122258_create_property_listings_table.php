814fd41879587a15fb3769dc508090db8427ad58
gpee <davidmogaji010@gmail.com>
2021-01-03 13:41:53 +0100

    created and ran properpert listing and property image migrations

--- database/migrations/2021_01_03_122258_create_property_listings_table.php
+++ database/migrations/2021_01_03_122258_create_property_listings_table.php
@@ -15,6 +15,21 @@ class CreatePropertyListingsTable extends Migration
     {
         Schema::create('property_listings', function (Blueprint $table) {
             $table->id();
+            $table->string('title', 100);
+            $table->text('description');
+            $table->string('address', 200);
+            $table->unsignedBigInteger('bedroom')->nullable;
+            $table->unsignedBigInteger('bathroom');
+            $table->unsignedBigInteger('toilet');
+            $table->unsignedBigInteger('slot');
+            $table->unsignedBigInteger('duration');
+            $table->decimal('purchase_amount', 8, 2);
+            $table->decimal('mngt_fee', 8, 2);
+            $table->decimal('sell_off_price', 8, 2);
+            $table->unsignedBigInteger('sell_off_profit_percent');
+            $table->decimal('rentage_price', 8, 2)->nullable();
+            $table->boolean('is_rentable', 1);
+            $table->boolean('is_active', 1)->default(1);
             $table->timestamps();
         });
     }
---
+++ database/migrations/2021_01_03_123656_create_property_images_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePropertyImagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('property_images', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('property_listing_id');
+            $table->foreign('property_listing_id')->references('id')->on('property_listings')->cascade('delete');
+            $table->string('thumbnail');
+            $table->boolean('is_active', 1)->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('property_images');
+    }
+}
