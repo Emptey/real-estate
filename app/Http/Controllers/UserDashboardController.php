2a94ef594ee967bd3453fd1733bfa7e14d1fe991
gpee <davidmogaji010@gmail.com>
2021-02-16 17:18:10 +0100

    populated user dashboard with some data

--- app/Http/Controllers/UserDashboardController.php
+++ app/Http/Controllers/UserDashboardController.php
@@ -3,13 +3,24 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\UserInvestment;
 
 class UserDashboardController extends Controller
 {
     // get index 
     public function index (Request $request) {
-       
+        // get user investment
+        $get_investment = UserInvestment::where('user_id', \Auth::user()->id)->get();
+
+        // check if investment record exist
+        if ($get_investment->count() > 0) {
+            // record found
+            $get_investment = $get_investment;
+        } else {
+            // record not found.
+            $get_investment = null;
+        }
         // return view
-        return view('v1.user.authenticated.index');
+        return view('v1.user.authenticated.index', ['user_investment' => $get_investment]);
     }
 }
--- resources/views/v1/user/authenticated/index.blade.php
+++ resources/views/v1/user/authenticated/index.blade.php
@@ -9,15 +9,25 @@
                 <div class="row">
                     <div class="col-md-6 col-lg-6 col-sm-12">
                         <div class="small-box">
-                            <h2 class="box-title-2">Title</h2>
-                            <h2 class="box-title">40</h2>
+                            <h2 class="box-title-2">Investment</h2>
+                            <h2 class="box-title">
+                                {{ !is_null($user_investment) ? $user_investment->count() : '0' }}
+                            </h2>
                         </div>
                     </div>
                     <div class="col-md-6 col-lg-6 col-sm-12">
                         <div class="small-box">
-                            <h2 class="box-title-2">Title</h2>
+                            <h2 class="box-title-2">Sell return</h2>
                             
-                            <h2 class="box-title">39</h2>
+                            @if(!is_null($user_investment))
+                                <?php $counter = 0; ?>
+                                @foreach($user_investment as $user_investments)
+                                    <?php $counter += $user_investments->investment->property_listing->sell_off_profit_percent; ?>
+                                @endforeach
+                                <h2 class="box-title">{{ $counter }}%</h2>
+                            @else
+                                <h2 class="box-title">0%</h2>
+                            @endif
                         </div>
                     </div>
                 </div>
@@ -25,8 +35,16 @@
         </div>
         <div class="col-md-4 col-lg-4 col-sm-12">
             <div class="small-box">
-                <h2 class="box-title-2">Transactions</h2>
-                <h2 class="box-title">39</h2>
+                <h2 class="box-title-2">Rentage return</h2>
+                @if(!is_null($user_investment))
+                    <?php $counter = 0; ?>
+                    @foreach($user_investment as $user_investments)
+                        <?php $counter += $user_investments->investment->property_listing->rentage_profit_percent; ?>
+                    @endforeach
+                    <h2 class="box-title">{{ $counter }}%</h2>
+                @else
+                    <h2 class="box-title">0%</h2>
+                @endif
             </div>
         </div>
     </div>
@@ -34,7 +52,7 @@
     <div class="row">
         <div class="col-md-8 col-lg-8 col-sm-12">
             <div class="small-box">
-                <h2 class="box-title-2">Chart 1</h2>
+                <h2 class="box-title-2">Investment Rate</h2>
                 <canvas id="myChart" width="300" height="100"> </canvas> 
 
                 <!-- <script>
@@ -45,12 +63,12 @@
         <div class="col-md-4 col-lg-4 col-sm-12">
             <div class="small-box fit-parent">
                 <h2 class="box-title-2">
-                    Something
+                    Investment status
                 </h2>
 
-                <h2 class="box-title">
-                    25
-                </h2>
+                <div class="mt-4">
+                    <canvas id="status" width="300" height="200"> </canvas> 
+                </div>
             </div>
         </div>
     </div>
@@ -58,7 +76,7 @@
     <div class="row">
         <div class="col-md-12 col-lg-12 col-sm-12">
             <div class="small-box">
-                <h2 class="">Chart 2</h2>
+                <h2 class="box-title-2">Investment pay rate</h2>
                 <canvas id="myChart2" width="300" height="100"> </canvas> 
             </div>
         </div>
